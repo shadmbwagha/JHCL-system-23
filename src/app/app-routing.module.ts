c16cd6e550b82ec9c90f132b423d5ee4b9ffc8a5
shad mbwagha <shadracksylvestar@gmail.com>
2023-08-29 13:04:55 +0300

    orga

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrganogramComponent } from './organogram/organogram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/organo',
+    component: OrganogramComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DiagramAllModule } from '@syncfusion/ej2-angular-diagrams';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +14,7 @@ import { OrganogramComponent } from './organogram/organogram.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DiagramAllModule,
+    DiagramModule,
     BrowserAnimationsModule
   ],
   providers: [],
